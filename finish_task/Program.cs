e23841ad9b841c8fbcdbd669324fc3d32f9545a9
Yaroslav <selutin.ya@yandex.ru>
2022-09-15 11:59:15 +0300

    finish task done

--- finish_task/Program.cs
+++ finish_task/Program.cs
@@ -1,4 +1,27 @@
-﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+﻿
+// Написать программу, которая из имеющегося массива строк формирует массив из строк,
 // длина которых меньше либо равна 3 символам.
 
-Console.WriteLine("Hello!");
+Console.Clear();
+
+Console.Write($"Задайте массив, вводя данные через запятую: ");
+string info = Console.ReadLine();
+string[] arr = info.Split(',').ToArray();
+
+Console.Write($"[{string.Join(" ", arr)}] -> ");
+GetArray(arr);
+
+
+void GetArray(string[] array)
+{
+    Console.Write("[");
+    string[] array2 = new string[array.Length];
+    for (int i = 0; i < array2.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            Console.Write($"{array[i]} ");
+        }
+    }
+    Console.WriteLine("]");
+}
